06ad68d5547111d9184882af13c8c33472b8ccf2
leelawd <leelawd123@gmail.com>
2019-02-18 04:19:03 +0000

    2.0.3

--- src/main/java/net/moddedminecraft/mmcrules/Main.java
+++ src/main/java/net/moddedminecraft/mmcrules/Main.java
@@ -44,7 +44,7 @@ import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-@Plugin(id = "mmcrules", name = "MMCRules", version = "2.0.2", description = "Force players to read and accept the rules.")
+@Plugin(id = "mmcrules", name = "MMCRules", version = "2.0.3", description = "Force players to read and accept the rules.")
 public class Main {
 
     @Inject
